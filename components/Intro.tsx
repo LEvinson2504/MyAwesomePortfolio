e568726808d3c0a96e2f0ab814e382f9b4708b56
LEvinson2504 <ldsouza2504@gmail.com>
2020-10-14 11:25:03 +0530

    removed xl breakpoint

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Intro() {
     return <div className="container sm:min-h-screen sm:w-screen h-64">
-        <div className="bg-blue-100 -mt-20 ml-10 sm:min-h-screen h-screen sm:ml-40 sm:-mt-5 xl:w-140">
+        <div className="bg-blue-100 -mt-20 ml-10 sm:min-h-screen h-screen sm:ml-40 sm:-mt-5">
             <div className="sm:absolute bottom-0 -ml-10 mt-20">
                 <h1 className="text-4xl sm:text-6xl font-light">Levinson D'souza</h1>
                 <div className="flex flex-row">
@@ -11,7 +11,7 @@ export default function Intro() {
                 </div>
             </div>
             <div className="sm:absolute sm:bottom-0 right-0 sm:-mb-20 sm:mr-10 sm:w-64 sm:mt-20 mt-10">
-                <p className="text-2xl sm:text-4xl xl:text-6xl sm:w-600 xl:w-80 sm:mb-40 -ml-5 font-light">
+                <p className="text-2xl sm:text-4xl sm:w-600 sm:mb-40 -ml-5 font-light">
                     I’m  a web developer based in Mumbai who likes to work and keep up with the latest trends in web development.
                 </p>
             </div>
