a53a29e2fd880cbda232e246814d467d933bce98
LEvinson2504 <ldsouza2504@gmail.com>
2020-10-19 11:42:51 +0530

    Links are working now

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,41 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Footer() {
-    return <section className="sm:ml-40 flex flex-row">
-        <div className="flex flex-col sm:ml-20 sm:my-10">
-            <h3 className="text-4xl font-light">Home</h3>
-            <h3 className="text-4xl font-light">About</h3>
-            <h3 className="text-4xl font-light">Projects</h3>
-        </div>
+  return (
+    <section className="sm:ml-40 flex flex-row">
+      <div className="flex flex-col sm:ml-20 sm:my-10">
+        <h3 className="text-4xl font-light">
+          <Link href="/">Home</Link>
+        </h3>
+        <h3 className="text-4xl font-light">
+          <Link href="/">About</Link>
+        </h3>
+        <h3 className="text-4xl font-light">
+          <Link href="/projects">Projects</Link>
+        </h3>
+      </div>
 
-        <div className="flex flex-col sm:ml-40 sm:my-10">
-            <h3 className="text-4xl font-light">LinkedIn</h3>
-            <h3 className="text-4xl font-light">Github</h3>
-            <h3 className="text-4xl font-light">Twitter</h3>
-        </div>
+      <div className="flex flex-col sm:ml-40 sm:my-10">
+        <h3 className="text-4xl font-light">
+          <a
+            href="https://www.linkedin.com/in/levinson-d-souza-093b61129/"
+            target="_blank"
+          >
+            LinkedIn
+          </a>
+        </h3>
+        <h3 className="text-4xl font-light">
+          <a href="https://github.com/LEvinson2504/" target="_blank">
+            Github
+          </a>
+        </h3>
+        <h3 className="text-4xl font-light">
+          <a href="https://twitter.com/levinsondsouza" target="_blank">
+            Twitter
+          </a>
+        </h3>
+      </div>
     </section>
+  );
 }
--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import Link from "next/link";
 
 export default function TopNav() {
-    return <header className="bg-white">
-        <div className="sm:flex sm:justify-end sm:flex-row text-right sm:mx-5 p-4">
-            <div className="sm:p-5 text-gray-700 text-3xl sm:text-4xl">Home</div>
-            <div className="sm:p-5 text-gray-700 text-3xl sm:text-4xl">About Me</div>
-            <div className="sm:p-5 text-gray-700 text-3xl sm:text-4xl">Projects</div>
+  return (
+    <header className="bg-white">
+      <div className="sm:flex sm:justify-end sm:flex-row text-right sm:mx-5 p-4">
+        <div className="sm:p-5 text-gray-700 text-3xl sm:text-4xl">
+          <Link href="/">Home</Link>
         </div>
+        <div className="sm:p-5 text-gray-700 text-3xl sm:text-4xl">
+          <Link href="/">About Me</Link>
+        </div>
+        <div className="sm:p-5 text-gray-700 text-3xl sm:text-4xl">
+          <Link href="/projects">Projects</Link>
+        </div>
+      </div>
     </header>
+  );
 }
